a4dcada36358db521b411e07375da00c59234041
Sébastien Pittet <sebastien@pittet.org>
2022-05-05 17:25:45 +0200

    reboot after AD setup

--- powershell-scripts/run_01.ps1
+++ powershell-scripts/run_01.ps1
@@ -6,26 +6,6 @@ Set-LocalUser `
  -Password $my_secure_password `
  -PasswordNeverExpires $true
 
-Install-windowsfeature AD-Domain-Services -IncludeManagementTools
-
-Install-ADDSForest `
--CreateDnsDelegation:$false `
--DatabasePath “C:\Windows\NTDS” `
--DomainMode “Win2012R2” `
--DomainName “yourdomain.internal” `
--DomainNetbiosName “YOURDOMAIN” `
--ForestMode “Win2012R2” `
--InstallDns:$true `
--LogPath “C:\Windows\NTDS” `
--NoRebootOnCompletion:$true `
--SysvolPath “C:\Windows\SYSVOL” `
--Force:$true `
--SafeModeAdministratorPassword $my_secure_password
-
-New-ADComputer `
- -Name "FS01" `
- -AccountPassword $my_secure_password
-
 # Configure next step (run_02) before reboot
 Invoke-WebRequest `
   -Uri "https://raw.githubusercontent.com/SebastienPittet/lametric-ssl-expiry/master/requirements.txt" `
@@ -37,4 +17,20 @@ New-ItemProperty `
   -Value "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe ""C:\Program Files\EXOSCALE\run_02.ps1"" " `
   -PropertyType "String"
 
-Restart-Computer
+Install-windowsfeature AD-Domain-Services -IncludeManagementTools
+
+Install-ADDSForest `
+ -CreateDnsDelegation:$false `
+ -DatabasePath "C:\Windows\NTDS" `
+ -DomainMode "Win2012R2" `
+ -DomainName "exoscale.local" `
+ -DomainNetbiosName "EXOSCALE" `
+ -ForestMode "Win2012R2" `
+ -InstallDns:$true `
+ -LogPath "C:\Windows\NTDS" `
+ -NoRebootOnCompletion:$false `
+ -SysvolPath "C:\Windows\SYSVOL" `
+ -Force:$true `
+ -SafeModeAdministratorPassword $my_secure_password
+
+
