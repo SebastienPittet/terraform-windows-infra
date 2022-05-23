439a8dfa62fcf75d8c7d2f61866f40343da32fc2
Sébastien Pittet <sebastien@pittet.org>
2022-05-24 01:45:51 +0200

    Lausanne

--- powershell-scripts/run_01.ps1
+++ powershell-scripts/run_01.ps1
@@ -9,3 +9,18 @@ New-ItemProperty `
   -Value "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe -File ""C:\Program Files\EXOSCALE\run_02.ps1"" " `
   -PropertyType "String"
 
+  $privIntAlias = (Get-NetIPAddress -IPAddress "10.0.0.20" | Select-Object -Property InterfaceAlias).InterfaceAlias
+  Set-DnsClientServerAddress -InterfaceAlias $privIntAlias -ServerAddresses 1.1.1.1
+  
+  
+  Install-WindowsFeature DHCP `
+    -IncludeManagementTools
+  Restart-Service dhcpserver
+  Add-DhcpServerInDC -DnsName DC01.exoscale.internal -IPAddress 10.0.0.20
+  Set-ItemProperty –Path registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\ServerManager\Roles\12 –Name ConfigurationState –Value 2
+  Add-DhcpServerv4Scope -name "Exoscale" -StartRange 10.0.0.100 -EndRange 10.0.0.150 -SubnetMask 255.255.255.0 -State Active
+  Add-DhcpServerv4ExclusionRange -ScopeID 10.0.0.0 -StartRange 10.0.0.1 -EndRange 10.0.0.100
+  Set-DhcpServerv4OptionValue -OptionID 3 -Value 10.0.0.1 -ScopeID 10.0.0.0 -ComputerName DC01.exoscale.internal
+  Set-DhcpServerv4OptionValue -DnsDomain exoscale.internal -DnsServer 10.0.0.20
+  
+  
